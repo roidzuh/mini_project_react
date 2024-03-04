74c2ab58420d9db469bf287a3685418a3d90541e
roid zuhdianto <roidzuh@gmail.com>
2024-03-04 21:23:45 +0700

    styling the MainNav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,19 +1,61 @@
 import { NavLink } from "react-router-dom";
+import {
+  HiOutlineHome,
+  HiOutlineUserPlus,
+  HiOutlineCog6Tooth,
+} from "react-icons/hi2";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #505050;
+  padding: 1.2rem 3rem;
+  transition: all 0.3s ease-in-out;
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  &:hover,
+  &:active,
+  &.active:link {
+    color: #0e0e0e;
+    background-color: #f0f0f0;
+    border-radius: 0.4rem;
+  }
+`;
 
 const MainNav = () => {
   return (
     <nav>
-      <ul>
+      <StyledUl>
         <li>
-          <NavLink>Home</NavLink>
+          <StyledNavLink to="/dashboard">
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
         </li>
         <li>
-          <NavLink>Users</NavLink>
+          <StyledNavLink to="/users">
+            <HiOutlineUserPlus />
+            <span>Users</span>
+          </StyledNavLink>
         </li>
         <li>
-          <NavLink>Settings</NavLink>
+          <StyledNavLink to="/settings">
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
         </li>
-      </ul>
+      </StyledUl>
     </nav>
   );
 };
