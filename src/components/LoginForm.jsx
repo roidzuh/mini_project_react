e7d2100151343137bb1322708cb5911b9481321c
roid zuhdianto <roidzuh@gmail.com>
2024-03-04 21:38:57 +0700

    create Login Form comp

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+const LoginForm = () => {
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input type="email" placeholder="Email" />
+      <label htmlFor="password">Password</label>
+      <input type="password" placeholder="Password" />
+    </form>
+  );
+};
+
+export default LoginForm;
