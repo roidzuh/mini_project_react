b3333674e8e06e80b1e9d099c61e2c94e14ba508
roid zuhdianto <roidzuh@gmail.com>
2024-03-05 09:13:14 +0700

    styling Login page and LoginForm comp

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,46 @@
+import styled from "styled-components";
+
+const FormStyled = styled.form`
+  width: 50%;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 2.4rem 4rem;
+  border-radius: 0.5rem;
+`;
+
+const WrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  padding: 1.2rem 0;
+`;
+
+const LabelStyled = styled.label`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #505050;
+`;
+
+const InputStyled = styled.input`
+  border: 1px solid #ccc;
+  background-color: #f5f5f5;
+  border-radius: 0.2rem;
+  padding: 0.8rem 1.2rem;
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+`;
+
 const LoginForm = () => {
   return (
-    <form>
-      <label htmlFor="email">Email</label>
-      <input type="email" placeholder="Email" />
-      <label htmlFor="password">Password</label>
-      <input type="password" placeholder="Password" />
-    </form>
+    <FormStyled>
+      <WrapperStyled>
+        <LabelStyled htmlFor="email">Email address</LabelStyled>
+        <InputStyled type="email" placeholder="Email" />
+      </WrapperStyled>
+      <WrapperStyled>
+        <LabelStyled htmlFor="password">Password</LabelStyled>
+        <InputStyled type="password" placeholder="Password" />
+      </WrapperStyled>
+    </FormStyled>
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import Logo from "../components/Logo";
+import LoginForm from "../components/LoginForm";
+
+const LoginStyled = styled.main`
+  background-color: #eeeeee;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  min-height: 100vh;
+`;
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginStyled>
+      <Logo />
+      <h1>Login to your account</h1>
+      <LoginForm />
+    </LoginStyled>
+  );
 };
 
 export default Login;
