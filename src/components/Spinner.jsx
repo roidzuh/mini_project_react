4ec24f806c58f58350aa72b63abcdcea312c93b2
roid zuhdianto <roidzuh@gmail.com>
2024-03-05 10:32:21 +0700

    create Spinner comp

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,26 @@
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Loader = styled.div`
+  width: 15px;
+  height: 15px;
+  border: 5px solid #fff;
+  border-bottom-color: #ff3d00;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: ${rotate} 1s linear infinite;
+`;
+const Spinner = () => {
+  return <Loader />;
+};
+
+export default Spinner;
