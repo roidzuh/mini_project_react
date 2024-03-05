b8431898877d17df5c2e94db7eaa407c37e98903
roid zuhdianto <roidzuh@gmail.com>
2024-03-05 13:53:03 +0700

    configure react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import { routeList } from "./Route/routes";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const element = useRoutes(routeList);
-  return element;
+  return (
+    <>
+      {element}
+      <Toaster />
+    </>
+  );
 };
 
 export default App;
