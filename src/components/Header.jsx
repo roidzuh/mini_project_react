12ac3f483a3d2779801c1c898b40a6ba627f2b49
roid zuhdianto <roidzuh@gmail.com>
2024-03-06 10:31:41 +0700

    build Header and update styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
+import UserAvatar from "./UserAvatar";
+import HeaderMenu from "./HeaderMenu";
 
 const StyledHeader = styled.header`
   background-color: #fff;
   padding: 1rem 4rem;
   border-bottom: 1px solid #ccc;
+
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 1rem;
 `;
 const Header = () => {
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <UserAvatar />
+      <HeaderMenu />
+    </StyledHeader>
+  );
 };
 
 export default Header;
