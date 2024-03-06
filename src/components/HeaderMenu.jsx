0e2bcf3832fb6eba2f224fb0b20dec38972a4115
roid zuhdianto <roidzuh@gmail.com>
2024-03-06 10:30:48 +0700

    create HeaderMenu comp

---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,31 @@
+import { HiOutlineArrowRightOnRectangle } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeaderMenu = styled.button`
+  font-size: 2rem;
+  color: #505050;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    color: #0e0e0e;
+  }
+`;
+
+const HeaderMenu = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    navigate("/login");
+  };
+
+  return (
+    <StyledHeaderMenu onClick={handleLogout}>
+      <HiOutlineArrowRightOnRectangle />
+    </StyledHeaderMenu>
+  );
+};
+
+export default HeaderMenu;
