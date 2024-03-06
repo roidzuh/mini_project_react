1aedebbca7cf649d5f6771a455ee9cc666b36383
roid zuhdianto <roidzuh@gmail.com>
2024-03-06 18:00:17 +0700

    update style

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,35 @@
 import axios from "axios";
 import Button from "./Button";
 import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import User from "./User";
+
+const StyledH1 = styled.h1`
+  margin-bottom: 1rem;
+`;
+
+const StyledContainer = styled.div`
+  background-color: #ffffff;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+  border: 1px solid #ccc;
+  margin-bottom: 2rem;
+`;
+
+const StyledUserList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  padding: 2rem;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+`;
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -44,28 +73,28 @@ const UserList = () => {
   }, [getUserList]);
 
   return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <img src={user.avatar} alt={user.first_name} />
-          <p>
-            {user.first_name} {user.last_name}
-          </p>
-          <p>{user.email}</p>
-        </div>
-      ))}
-      <div>
-        <Button onClick={handleBack} disabled={pagination.page === 1}>
-          Back
-        </Button>
-        <Button
-          onClick={handleNext}
-          disabled={pagination.page === pagination.total_pages}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
+    <>
+      <StyledH1>Dashboard</StyledH1>
+      <StyledContainer>
+        <StyledUserList>
+          {users.map((user) => (
+            <User key={user.id} user={user} />
+          ))}
+        </StyledUserList>
+        <StyledDiv>
+          <Button onClick={handleBack} disabled={pagination.page === 1}>
+            Back
+          </Button>
+          <span>{pagination.page}</span>
+          <Button
+            onClick={handleNext}
+            disabled={pagination.page === pagination.total_pages}
+          >
+            Next
+          </Button>
+        </StyledDiv>
+      </StyledContainer>
+    </>
   );
 };
 
