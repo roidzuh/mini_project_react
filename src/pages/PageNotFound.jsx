22ab329a2ea41609bcbda9eefb27f31a027ef6a8
roid zuhdianto <roidzuh@gmail.com>
2024-03-12 13:00:56 +0700

    update Styling

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+import WafeContainer from "../components/WafeContainer";
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const NotFoundTitle = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const NotFoundMessage = styled.p`
+  font-size: 1rem;
+  text-align: center;
+`;
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <WafeContainer>
+      <NotFoundContainer>
+        <NotFoundTitle>404 - Page Not Found</NotFoundTitle>
+        <NotFoundMessage>
+          The page you are looking for might have been removed or is temporarily
+          unavailable.
+        </NotFoundMessage>
+      </NotFoundContainer>
+    </WafeContainer>
+  );
 };
 
 export default PageNotFound;
