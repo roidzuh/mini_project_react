e011beccc2b4a03eb8a3ac3d1e19988df64bb806
roid zuhdianto <roidzuh@gmail.com>
2024-03-13 15:47:53 +0700

    update avatar

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
@@ -8,43 +6,16 @@ const StyledContainer = styled.div`
   gap: 1rem;
 `;
 
-const StyledImg = styled.img`
-  width: 3rem;
-  height: 3rem;
-  border-radius: 100%;
-`;
-
 const StyledSpan = styled.span`
-  font-weight: bold;
-  font-size: 1.2rem;
   color: #505050;
 `;
 
 const UserAvatar = () => {
-  const [userData, setUserData] = useState(null);
-  const getUserData = () => {
-    axios
-      .get("https://reqres.in/api/users/2")
-      .then((res) => {
-        setUserData(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
-  };
-
-  useEffect(() => {
-    getUserData();
-  }, []);
+  const user = localStorage.getItem("user");
 
   return (
     <StyledContainer>
-      <div>
-        <StyledImg src={userData?.avatar} alt={userData?.first_name} />
-      </div>
-      <StyledSpan>
-        {userData?.first_name} {userData?.last_name}
-      </StyledSpan>
+      <StyledSpan>{user}</StyledSpan>
     </StyledContainer>
   );
 };
