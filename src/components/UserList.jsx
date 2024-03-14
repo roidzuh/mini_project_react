569763eb853618c47c737c1807f6015bd3260284
roid zuhdianto <roidzuh@gmail.com>
2024-03-14 09:49:59 +0700

    update grid

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -11,7 +11,7 @@ const StyledH1 = styled.h1`
 
 const StyledUserList = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
   gap: 2rem;
   padding: 2rem;
 `;
